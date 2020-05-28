d211c49bdea18d4cc795170a1f6dac5f405cbfcb
Daria <andronikova.daria@gmail.com>
2020-05-28 21:30:34 +0300

    rebalance load portfolio function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3 as sql
 import math
 
 
-from helpers import apiprice, error_page
+from helpers import apiprice, error_page, load_portfolio
 
 app = Flask(__name__)
 
@@ -12,46 +12,23 @@ userid = 1 #TODO - download from session
 
 @app.route('/', methods=['GET'])
 def index_page_landing():
+    portfolio = load_portfolio(userid, DATABASE)
 
-    with sql.connect(DATABASE) as con:
-        # to have result of .execute as dictionary
-        con.row_factory = sql.Row
-        cur = con.cursor()
+    # for new user
+    if portfolio is None:
+        return render_template('index_newuser.html')
 
-        cur.execute("SELECT * FROM portfolio WHERE userid == :userid", {"userid":userid})
-        rows = cur.fetchall()
-
-        # for new user
-        if len(rows) == 0:
-
-            return render_template('index_newuser.html')
-
-        # for user WITH PORTFOLIO
-        i = 0
-        row_api = {}
-        total = 0 # for whole portfolio
-        for row in rows:
-            res = apiprice(row['ticker'])
-            if res is not None:
-                row_api[i] = {'fullName': res['name'], 'price': res['price'],'fullPrice' : res['price'] * row['number']}
-                total += row_api[i]['fullPrice']
-                i += 1
-            else:
-                error_page('Could not load price')
-
-        # real fraction calculation
-        for j in range(i):
-            row_api[j]["realFraction"] = math.floor(100 * row_api[j]['fullPrice'] / total)
-
-        return render_template('index.html', row=rows, row_api=row_api,length=i, total=total)
-        con.close()
+    # for user WITH PORTFOLIO
+    return render_template('index.html', portfolio=portfolio)
 
 @app.route('/rebalance', methods=['GET','POST'])
 def rebalance_page():
     if request.method == "GET":
         return render_template("rebalance.html")
 
-
+@app.route('/books/<genre>')
+def books(genre):
+    return "All Books in {} category".format(genre)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
-import os, urllib.parse, requests
+import os, urllib.parse, requests, math
+import sqlite3 as sql
 
 def apiprice(ticker):
     # load price from NY
@@ -23,4 +24,37 @@ def apiprice(ticker):
 def error_page(message):
     return render_template("error_page.html",message=message)
 
+def load_portfolio(userid, database):
+    # to load portfolio data from db and comine in with results of API query
+    with sql.connect(database) as con:
+        # to have result of .execute as dictionary
+        con.row_factory = sql.Row
+        cur = con.cursor()
 
+        cur.execute("SELECT * FROM portfolio WHERE userid == :userid", {"userid":userid})
+        rows = cur.fetchall()
+
+        # for new user
+        if len(rows) == 0:
+            return None
+
+        # for user WITH PORTFOLIO
+        i = 0
+        portfolio = {}
+        total = 0 # for whole portfolio
+        for row in rows:
+            res = apiprice(row['ticker'])
+            if res is not None:
+                portfolio[i] = {'fullName': res['name'], 'price': res['price'],'fullPrice' : res['price'] * row['number'],'ticker' : row['ticker'], 'number' : row['number'], 'fraction' : row['fraction']}
+
+                total += portfolio[i]['fullPrice']
+                i += 1
+            else:
+                error_page('Could not load price')
+
+        # real fraction calculation
+        for j in range(i):
+            portfolio[j]["realFraction"] = math.floor(100 * portfolio[j]['fullPrice'] / total)
+
+    con.close()
+    return portfolio
