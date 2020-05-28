178d7f76fc7858939a57e1318261cfb70ac02623
Daria <andronikova.daria@gmail.com>
2020-05-28 11:41:23 +0300

    calculte real_fraction index_newuser rebalance

--- app.py
+++ app.py
@@ -1,56 +1,57 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, request
 import sqlite3 as sql
+import math
+
 
 from helpers import apiprice, error_page
 
 app = Flask(__name__)
 
 DATABASE = 'portfolio.db'
-
-# def get_db():
-#     db = getattr(g, '_database',None)
-#     if db is None:
-
-
-#con = sqlite3.connect('portfolio.db')
-# cursor = con.cursor()
-# print(cursor.execute("SELECT * FROM users"))
-# con.commit()
-# con.close()
-
-# session.user_id = True
+userid = 1 #TODO - download from session
 
 @app.route('/', methods=['GET'])
 def index_page_landing():
-    userid = 1 #TODO - download from session
+
     with sql.connect(DATABASE) as con:
         # to have result of .execute as dictionary
         con.row_factory = sql.Row
         cur = con.cursor()
 
-        cur.execute("SELECT * FROM portfolio")
+        cur.execute("SELECT * FROM portfolio WHERE userid == :userid", {"userid":userid})
         rows = cur.fetchall()
 
-        # write results of api query in dictionary
+        # for new user
+        if len(rows) == 0:
+
+            return render_template('index_newuser.html')
+
+        # for user WITH PORTFOLIO
         i = 0
         row_api = {}
-        total = 0
+        total = 0 # for whole portfolio
         for row in rows:
             res = apiprice(row['ticker'])
             if res is not None:
                 row_api[i] = {'fullName': res['name'], 'price': res['price'],'fullPrice' : res['price'] * row['number']}
-
                 total += row_api[i]['fullPrice']
                 i += 1
             else:
                 error_page('Could not load price')
 
-        return render_template('index.html', row=rows, row_api=row_api,length=i)
+        # real fraction calculation
+        for j in range(i):
+            row_api[j]["realFraction"] = math.floor(100 * row_api[j]['fullPrice'] / total)
+
+        return render_template('index.html', row=rows, row_api=row_api,length=i, total=total)
         con.close()
 
-
+@app.route('/rebalance', methods=['GET','POST'])
+def rebalance_page():
+    if request.method == "GET":
+        return render_template("rebalance.html")
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
