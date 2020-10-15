03ac1030806e17c4b78f15cb57a4638608bb9cb3
Daria <andronikova.daria@gmail.com>
2020-10-15 13:23:39 +0200

    portfolio_db deleted

--- models.py
+++ models.py
@@ -2,17 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class portfolio_db(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    userid = db.Column(db.Integer())
-    ticker = db.Column(db.String(64))
-    number = db.Column(db.Integer())
-    fraction = db.Column(db.Integer())
-
-    def __repr__(self):
-        return '<portfolio_db {}>'.format(self.ticker)
-
-
 class ticker_db(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     userid = db.Column(db.Integer())
