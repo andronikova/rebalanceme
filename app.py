316f82c9c2609d78012cf22652f1983d9911bcbf
Daria <andronikova.daria@gmail.com>
2020-12-16 17:02:55 +0100

    footer

--- app.py
+++ app.py
@@ -826,6 +826,10 @@ def testaccount():
         return redirect('/')
 
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
