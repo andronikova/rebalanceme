369e333d9e9405de10ce7e59538fec8bc0b1ba99
Daria <andronikova.daria@gmail.com>
2021-07-16 11:31:13 +0300

    heroku

--- helpers.py
+++ helpers.py
@@ -12,7 +12,7 @@ def apiprice_marketstack(ticker):
 
     except requests.RequestException:
         print(f'exception in loading price for {ticker} using marketstack')
-        print(response['error'])
+        print(f'response error is {response}')
         return None
 
     try:
